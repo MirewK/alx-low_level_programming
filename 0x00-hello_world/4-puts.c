680662d475d626b6e078e92fd479ae3eed38185b
MirewK <meremehertab@gmail.com>
2023-06-15 15:48:13 -0700

    puts betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
-*main - includes defines external declarations 
-*Return: Always 0 (success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ * /
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
