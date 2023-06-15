9d7de9a36e91eb635701328779cda6a0524f7766
MirewK <meremehertab@gmail.com>
2023-06-15 15:05:51 -0700

    putss

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
-/**
-*  main includes,
-*  
-*  defines external declarations 
-*/
+/** main includes,
+*  defines external declarations */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
