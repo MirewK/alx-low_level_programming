4ca0a70c5b7a0c4ffc29b95edfba089973a2dadb
MirewK <meremehertab@gmail.com>
2023-06-15 15:31:42 -0700

    puts finally

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
-* main - includes defines external declarations 
-* Return: Always 0 (success)
+*main - includes defines external declarations 
+*Return: Always 0 (success)
 */
 int main(void)
 {
