085a48052f4eba0b841f9e8d75daf78364f60772
MirewK <meremehertab@gmail.com>
2023-06-16 12:13:11 -0700

    tebahpla

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,23 +1,13 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (success)
+ *main - Prints the lower
+*Return: Always 0.
  */
 int main(void)
 {
-	int num;
-
-	for (num = 0; num <= 9; num++)
-	{
-		putchar((num % 10) + '0');
-		if(num == 9)
-			continue;
-
-		putchar(',');
-		putchar(' ');
-	}
+	char letter;
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
 	putchar('\n');
-
 	return (0);
 }
