9eb1093d77dcf02793197fcea3e8792502f599a1
MirewK <meremehertab@gmail.com>
2023-06-16 11:37:40 -0700

    add

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,10 +7,13 @@
  */
 int main(void)
 {
-	int num;
-	for (num = 0; num < 10;num++)
-		putchar((num % 10)+ '0');
-	putchar("\n");
+	int i = 48;
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
 	return (0);
 }
  
