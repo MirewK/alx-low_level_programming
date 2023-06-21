4063ec339b7eaa654e10568f213c6659b1d1df40
MirewK <meremehertab@gmail.com>
2023-06-21 12:45:33 -0700

    fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -19,9 +19,9 @@ int main(void)
 		fib2 = sum;
 	}
 	fib1_half1 = fib1 / 10000000000;
-tel:10000000000fib2_half1 = fib2 / 10000000000;
-tel:10000000000fib1_half2 = fib1 % 10000000000;
-tel:10000000000fib2_half2 = fib2 % 10000000000;
+	fib2_half1 = fib2 / 10000000000;
+	fib1_half2 = fib1 % 10000000000;
+	fib2_half2 = fib2 % 10000000000;
 
 	for (count = 93; count < 99; count++)
 	{
