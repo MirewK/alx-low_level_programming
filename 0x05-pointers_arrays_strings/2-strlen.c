293b4e343dac6f707b6bcf53311ad763bd6271d2
MirewK <meremehertab@gmail.com>
2023-06-26 13:13:45 -0700

    length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - gives the length of a string
+ *
+ * @s: pointer of the first character in the string
+ * Return: length of a string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+
+		return (i);
+}
