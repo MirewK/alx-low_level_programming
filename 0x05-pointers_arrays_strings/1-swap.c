3e14fe70df43cc56c591143ab4b4cd1d3c06aa36
MirewK <meremehertab@gmail.com>
2023-06-26 11:53:37 -0700

    swap two degits

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,10 +2,9 @@
 
 /**
  * swap_int - swaps the two numbers given
- *
- *@n: temporary variable to hold value of pointer a
  *@a: first digit on the left
  *@b: second digit on the right
+ *
  */
 
 void swap_int(int *a, int *b)
