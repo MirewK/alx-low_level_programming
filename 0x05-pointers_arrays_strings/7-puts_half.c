bb69f270b1d4374ffd4c2d9398a9e60ff1dd57cd
MirewK <meremehertab@gmail.com>
2023-06-27 07:56:00 -0700

    corrections

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,7 +11,6 @@ void puts_half(char *str)
 	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
-	{
 
 	if (i % 2 == 0)
 	{
@@ -26,7 +25,6 @@ void puts_half(char *str)
 		{
 			_putchar(str[i]);
 		}
-	}
 	}
 	_putchar('\n');
 }
