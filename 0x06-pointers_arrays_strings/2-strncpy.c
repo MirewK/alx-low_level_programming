c9250155224b3ce6e88d08046501e947c3cb4554
MirewK <meremehertab@gmail.com>
2023-06-30 07:05:36 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,10 +4,11 @@
 
 /**
  * _strncpy - function that copies a string
- * @*dest: destination string
- * @*src: source string
+ * @dest: destination string
+ * @src: source string
  * @n: length of the caracter to be copied
  *
+ * Return: destination string
  */
 
 char *_strncpy(char *dest, char *src, int n)
