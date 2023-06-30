0428204a680c51b25db7af0a748be49f208a8cc2
MirewK <meremehertab@gmail.com>
2023-06-30 08:17:05 -0700

    strncat correction

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,21 +14,23 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
+	char *s1 = dest;
+
 	if (dest == 0 || src == 0)
 	{
 		return 0;
 	}
-	while (*dest != '\0')
+	while (*s1 != '\0')
 	{
-		dest++;
+		s1++;
 	}
 	while (n--)
 	{
-		if (!(*dest++ = *src++))
+		if (!(*s1++ = *src++))
 		{
 			return dest;
 		}
 	}
-	*dest = '\0';
+	*s1 = '\0';
 	return (dest);
 }
