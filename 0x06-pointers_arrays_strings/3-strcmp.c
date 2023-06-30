122dbfa675a8553f202e1ee66a8ab62c70a55eb5
MirewK <meremehertab@gmail.com>
2023-06-30 15:30:36 -0700

    strncpy correction

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,21 +11,19 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int flag = 0;
-
-	while (*s1 != '\0' || *s2 != '\0')
+	while ((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
 	{
-		if (*s1 == *s2)
-		{
-			s1++;
-			s2++;
-		}
-		else if ((*s1 == '\0' && *s2 != '\0') || (*s1 != '\0' && *s2 == '\0') || *s1 != *s2)
-		{
-			flag = 1;
-			break;
-		}
+		s1++;
+		s2++;
 	}
-	if (flag == 0)
+
+	if (*s1 == *s2)
+	{
 		return (0);
+	}
+
+	else
+	{
+		return (*s1 - *s2);
+	}
 }
