c65fb039b56de9332d8d8dce050556980544c52e
MirewK <meremehertab@gmail.com>
2023-06-30 14:58:28 -0700

    strncpy correction

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,9 +16,9 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 	i = 0;
-	while (i <n && *(src + i))
+	while (i < n && *(src + i))
 	{
-		*(des + i) = *(src + i);
+		*(dest + i) = *(src + i);
 		i++;
 	}
 	while (i < n)
