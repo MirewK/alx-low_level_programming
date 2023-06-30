f17d5d1b1fc41f8bbb117f1479a1dcdefb5cdeed
MirewK <meremehertab@gmail.com>
2023-06-30 15:09:11 -0700

    strncpy correction

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * cap_string - changes all string into capital
+ * @s: string to be changed
+ *
+ * Return: string s in capital
+ */
+
+char *cap_string(char *s)
+{
+	int i = 0;
+	int j;
+
+	char a[] = "\t\n,;.!?\"(){}";
+
+	while (*(s + i))
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		{
+			if (i == 0)
+				*(s + i) -= 'a' - 'A';
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (s + i) -= 'a' - 'A';
+				}
+			}
+		}
+		i++;
+	}
+	return (s);
+}
