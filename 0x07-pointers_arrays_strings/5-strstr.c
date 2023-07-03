9e8f9908c7b29fd573b692d4b80a60baba9ef23c
MirewK <meremehertab@gmail.com>
2023-07-03 15:19:53 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strstr - finds the first occurance
+ * @haystack: the string to be checked in for the occurance
+ * @needle: the string to be found
+ *
+ * Return: always 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *a;
+	char *b;
+
+	while (*haystack != '\0')
+	{
+		a = haystack;
+		b = needle;
+		whiel (*haystack != '\0' && *b != '\0' && *haystack == *b)
+		{
+			haystack++;
+			b++;
+		}
+		if (!*b)
+			return (a);
+		haystack = a + 1;
+	}
+	return (0);
+}
