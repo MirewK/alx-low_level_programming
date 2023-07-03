539ca1b8c8e13d4ac4efe77af872c346347e0af9
MirewK <meremehertab@gmail.com>
2023-07-03 14:37:23 -0700

    strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to be segmented
+ * @accept: bytes accepted
+ *
+ * Return: the number of segmented bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, seg;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		seg = 1;
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if(*(s + i) == *(accept + j))
+			{
+				seg = 0;
+				break;
+			}
+		}
+		if (seg == 1)
+			break;
+	}
+	return (i);
+}
