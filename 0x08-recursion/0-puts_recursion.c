a6c902c1a39e6b8fc949cab9d6687e61ce11bd1b
MirewK <meremehertab@gmail.com>
2023-07-05 14:58:54 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,9 +7,10 @@
 
 void _puts_recursion(char *s)
 {
-	*s != '\0';
-	_putchar(*s);
-
-	_puts_recursion(s + 1);
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 	_putchar('\n');
 }
