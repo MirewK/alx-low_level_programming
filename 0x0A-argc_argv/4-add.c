b6f653d64b2644e61d116de0e7f3589e8b26f566
MirewK <meremehertab@gmail.com>
2023-07-08 08:39:19 -0700

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,8 +1,7 @@
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
+#include"main.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
 /**
  * main - prints the sum
