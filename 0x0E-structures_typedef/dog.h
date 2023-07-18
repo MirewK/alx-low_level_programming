c4b09709447937b57d2034c4c44e51bc383a3e75
MirewK <meremehertab@gmail.com>
2023-07-18 08:40:02 -0700

    structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,26 @@
+#ifndef HEADER
+#define HEADER
+/**
+ * struct dog - define the structure dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
 struct dog
 {
 	char *name;
-	float *age;
+	float age;
 	char *owner;
 };
-typedef struct dog dog;
+
+/**
+ * dog_t - the new type for dog
+ */
+
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+#endif
