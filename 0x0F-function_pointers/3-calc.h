060bbec1b3f160816aa72fc1bb0a096f7f4c024c
MirewK <meremehertab@gmail.com>
2023-07-19 17:17:20 -0700

    function pointers

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,4 +1,5 @@
-#define _HEADER_
+#ifndef HEADER_FILE
+#define HEADER_FILE
 /**
  * struct op - structure for operation
  * @op: the operator
@@ -17,3 +18,5 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
+
+#endif
