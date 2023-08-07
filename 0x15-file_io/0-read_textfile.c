8e1ea3a531ae5827c1dbcf2b773b81e08d7e341b
MirewK <meremehertab@gmail.com>
2023-08-07 14:24:19 -0700

    read text file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - read a text file and prints it to the POSIX
+ * @filename: pointer to the name of the file
+ * @letters: number of letters it should read and print
+ *
+ * Returns: 0 if file con not be opened or read or NULL or fails to write
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *fp;
+	char buf[letters];
+	size_t nread;
+
+	if (filename == NULL)
+		return (0);
+
+	fp = fopen(filename, "r");
+	if (fp == NULL)
+		return (0);
+
+	nread = fread(buf, 1, letters, fp);
+	fclose(fp);
+
+	if (nread == 0)
+		return (0);
+
+	if (write(STDOUT_FILENO, buf, nread) != nread)
+		return (0);
+
+	return (nread);
+}
