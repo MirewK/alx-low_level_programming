d91ee95d389ce686945280db946014bfbe4e5be4
MirewK <meremehertab@gmail.com>
2023-08-11 14:40:18 -0700

    append text to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,9 +23,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	if (text_content == NULL)
-	{
 		text_content = "";
-	}
+
 	while (text_content[len] != '\0')
 	{
 		len++;
