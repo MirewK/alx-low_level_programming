65cdae7674cf242e25715a05e8913992f2254ac5
MirewK <meremehertab@gmail.com>
2023-08-11 13:49:21 -0700

    read text file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,10 +18,14 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
+	{
 		text_content = "";
+	}
 
 	while (text_content[len] != '\0')
+	{
 		len++;
+	}
 
 	fpWrite = write(fp, text_content, len);
 
