5e4933593245b22d7f147758cf2717ee1a04a58d
MirewK <meremehertab@gmail.com>
2023-08-11 18:33:11 -0700

    elf header

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -28,9 +28,10 @@ void elf_check(unsigned char *e_ident)
 {
 	int i;
 
-	for (i = 0; i < 4; i++)
+	for (i = 0; i < EI_NIDENT; i++)
 	{
-		if (e_ident[i] != 127 && e_ident[i] != 'E' && e_ident[i] != 'L' && e_ident[i] != 'F')
+		if (e_ident[i] != ELFMAG[i])
+		//if (e_ident[i] != 127 && e_ident[i] != 'E' && e_ident[i] != 'L' && e_ident[i] != 'F')
 		{
 			dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
 			exit(98);
