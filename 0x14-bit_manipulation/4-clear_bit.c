f479665ccc2a76aa5b89beaa44e2ee8f4ef5d3aa
MirewK <meremehertab@gmail.com>
2023-09-01 07:55:10 -0700

    binary to uint corr

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -6,13 +6,12 @@
  *
  * Return: 1 on success, or -1 on error
  */
-
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
-	*n &= ~(1 << index);
+	*n &= ~ (1 << index);
 
 	return (1);
 }
