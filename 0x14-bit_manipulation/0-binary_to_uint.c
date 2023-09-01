8dbf92a69c4eeed71d7d5ac9993624712bc1469f
MirewK <meremehertab@gmail.com>
2023-09-01 07:52:16 -0700

    binary to uint corr

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
  * binary_to_uint - converts a binary num to unsigned int
  * @b: pointer to binary num location
  *
  * Return: 0 if it is NULL or the converted num on success
  */
+
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0, mult = 1;
