12cbf3ad9c37d523efd8c19db6652c1c908dc976
MirewK <meremehertab@gmail.com>
2024-01-11 13:35:12 -0800

    aum dlistint

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * sum_slistint - gives the sum of all the data in the doubly linked list
+ * @head: pointer to the head of the doubly linked list
+ *
+ * Return: sum of the doubly linked list data
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum;
+
+	sum = 0;
+
+	if (head != NULL)
+	{
+		while (head->prev != NULL)
+			head = head->prev;
+
+		while (head != NULL)
+		{
+			sum += head->n;
+			head = head->next;
+		}
+	}
+	return (sum);
+}
