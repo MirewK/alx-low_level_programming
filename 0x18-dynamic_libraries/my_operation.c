2c41a2db33565fa9be6903ccd6b19369a17414a9
MirewK <meremehertab@gmail.com>
2024-01-15 12:42:38 -0800

    operations dynamic librarys

---
+++ 0x18-dynamic_libraries/my_operation.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+/**
+ * add_num - add two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the sum of the two numbers
+ */
+int add_num(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub_num - subtracts two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the difference of the two numbers
+ */
+int sub_num(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul_num - multiply two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the product of the two numbers
+ */
+int mul_num(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div_num - divides two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the result
+ */
+int div_num(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod_num - add two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the modulo of the two numbers
+ */
+int mod_num(int a, int b)
+{
+	return (a % b);
+}
